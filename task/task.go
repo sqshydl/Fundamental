494c33c6309abb7700d4e5e09b0f6cda23d6f928
Raa <mochammadtora@gmail.com>
2025-06-04 17:57:54 +0700

    Sudah menambahkan read task

--- task/task.go
+++ task/task.go
@@ -70,3 +70,23 @@ func saveTasks(tasks []Task) error {
 
 	return os.WriteFile("storage/Task.json", data, 0644)
 }
+
+func readTasks() {
+	tasks := loadTasks()
+	if len(tasks) == 0 {
+		fmt.Println("Belum ada task.")
+		return
+	}
+
+	for _, task := range tasks {
+		status := "Belum selesai"
+		if task.Completed {
+			status = "Selesai"
+		}
+		fmt.Printf("ID: %d | Title: %s | Status: %s\n", task.Id, task.Title, status)
+	}
+}
+
+func deleteTask() {
+
+}
